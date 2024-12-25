7a213181e0e6a497408ccfa73c7901466e6ec8b5
Joshua S <johnsilverknight@gmail.com>
2024-12-25 02:29:41 -0600

    delete tests

--- OrthodoxApp/test.swift
+++
@@ -1,8 +0,0 @@
-//
-//  test.swift
-//  OrthodoxApp
-//
-//  Created by Joshua Stalinger on 12/25/24.
-//
-
-import Foundation
--- OrthodoxApp/testfile2.swift
+++
@@ -1,8 +0,0 @@
-//
-//  testfile2.swift
-//  OrthodoxApp
-//
-//  Created by Joshua Stalinger on 12/25/24.
-//
-
-import Foundation
