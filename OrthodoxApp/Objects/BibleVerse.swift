9c913a5ece8dee6d9e655d6040be9335f14ddc3a
Joshua S <johnsilverknight@gmail.com>
2025-01-03 02:06:05 -0600

    I believe I have finished implementation of a daily quote. I also started implementing a daily verse object, doubt I will send a notification for it. I am having to enter in a manual dictionary for the verses because there is no good Bible API surprisingly.

---
+++ OrthodoxApp/Objects/BibleVerse.swift
@@ -0,0 +1,8 @@
+//
+//  BibleVerse.swift
+//  OrthodoxApp
+//
+//  Created by Joshua Stalinger on 1/3/25.
+//
+
+import Foundation
---
+++ OrthodoxApp/Utilities/VerseProvider.swift
@@ -0,0 +1,8 @@
+//
+//  VerseProvider.swift
+//  OrthodoxApp
+//
+//  Created by Joshua Stalinger on 1/3/25.
+//
+
+import Foundation
